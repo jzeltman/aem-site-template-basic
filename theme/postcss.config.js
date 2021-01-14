3ed6fe9954ccf46ce105fd6317f27ae96c3ca096
Joshua Zeltman <joshua.zeltman@gmail.com>
2021-01-14 12:18:31 -0300

    Added new postcss plugins
    Wrote mixin for the grid
    
    CSS Build now functional

--- theme/postcss.config.js
+++ theme/postcss.config.js
@@ -1,10 +1,18 @@
 // postcss.config.js
+const generateGrid = require('./src/mixins/grid');
+
 module.exports = {
   plugins: {
     'postcss-easy-import': {
       prefix: '_'
     },
+    'postcss-mixins': {
+      mixins: {
+        'generate-grid': generateGrid,
+      }
+    },
     'postcss-nested': {},
+    'postcss-at-rules-variables': {},
     tailwindcss: {},
     autoprefixer: {},
   }
